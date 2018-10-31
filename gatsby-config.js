bc793805a66b2b18cf9e61a3f553d0a96ee09992
Samuel Akintunde <samakintunde37@gmail.com>
2018-10-31 22:13:29 +0100

    Changed manifest and site favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#147dff',
+        theme_color: '#147dff',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/samakintunde.ico', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
